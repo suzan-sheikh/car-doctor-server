492cacd6f10f5c0824391d085b0f9f8b14d2423d
suzan-sheikh <mdsuzanskh@gmail.com>
2024-05-06 22:43:07 +0600

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
@@ -8,44 +8,45 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'cars-doctor-74791.web.app', 'cars-doctor-74791.firebaseapp.com'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-
 
 // our middleware
 
 const logger = (req, res, next) => {
-  console.log('my create middleware, log info:', req.method, req.url);
+  console.log("my create middleware, log info:", req.method, req.url);
   next();
-}
+};
 const verifyToken = (req, res, next) => {
   const token = req.cookies?.token;
   // console.log('token in the middleware', token);
-  
+
   // no token handdle
-  if(!token){
-    return res.status(401).send({message: 'unauthorized access'})
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
   }
   // jwt verify
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if(err){
-      return res.status(401).send({message: 'unauthorized access'})
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
     }
     req.user = decoded;
     next();
-  })
+  });
   // next()
+};
+
+const cookeOption = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production'? true : false,
+  sameSite: process.env.NODE_ENV === 'production'? 'none' :  'strict',
 }
-
-
-
-
-
-
 
 app.get("/", (req, res) => {
   res.send("server is running");
@@ -55,8 +56,8 @@ app.listen(port, () => {
   console.log(`server  is running ${port}`);
 });
 
-const uri = "mongodb://localhost:27017";
-// const uri = "mongodb+srv://<username>:<password>@cluster0.ykkxidd.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ykkxidd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -72,26 +73,23 @@ async function run() {
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
 
-    
-    app.post('/jwt', async(req, res) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      console.log('user token', user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
-      res.cookie('token', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none'
-      })
-      .send({success: true});
-    })
+      console.log("user token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, cookeOption)
+        .send({ success: true });
+    });
 
-    app.post('/logout', async(req, res) => {
+    app.post("/logout", async (req, res) => {
       const user = req.body;
-      console.log('login out user', user);
-      res.clearCookie('token', {maxAge:0}).send({success: true})
-    })
-    
-   
+      console.log("login out user", user);
+      res.clearCookie("token", { ...cookeOption, maxAge: 0 }).send({ success: true });
+    });
+
     // get all service data from mongoDB
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
@@ -113,10 +111,10 @@ async function run() {
     // get data to mongoDB
     app.get("/bookings", verifyToken, async (req, res) => {
       console.log(req.query.email);
-      console.log('token owner info', req.user);
+      console.log("token owner info", req.user);
 
-      if(req.user.email !== req.query.email){
-        return res.status(403).send({message: 'forbidden access'})
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
 
       let query = {};
@@ -127,10 +125,6 @@ async function run() {
       res.send(result);
     });
 
-
-
-
-
     // delete data from mongoDB
     app.delete("/bookings/:id", async (req, res) => {
       const id = req.params.id;
@@ -139,12 +133,6 @@ async function run() {
       const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
-
-
-
-
-
-
 
     // update data from mongoBD
     app.patch("/bookings/:id", async (req, res) => {
@@ -160,9 +148,9 @@ async function run() {
     });
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
